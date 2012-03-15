bcddba97442961435f8ec13e35e9f9d025b18151
Romain Neutron <imprec@gmail.com>
2012-03-15 13:27:37 +0100

    Add DefaultMedia

---
+++ src/MediaVorus/Media/DefaultMedia.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Copyright (c) 2012 Romain Neutron
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+namespace MediaVorus\Media;
+
+use \Symfony\Component\HttpFoundation\File\File as SymfonyFile;
+
+/**
+ *
+ * @author      Romain Neutron - imprec@gmail.com
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+class DefaultMedia
+{
+
+  /**
+   *
+   * @var \Symfony\Component\HttpFoundation\File\File
+   */
+  protected $file;
+
+  public function __construct(\SplFileInfo $file)
+  {
+    if (!$file instanceof SymfonyFile)
+    {
+      $this->file = new SymfonyFile($file->getPathname());
+    }
+
+    return $this;
+  }
+
+  /**
+   *
+   * @return \Symfony\Component\HttpFoundation\File\File
+   */
+  public function getFile()
+  {
+    return $this->file;
+  }
+
+}

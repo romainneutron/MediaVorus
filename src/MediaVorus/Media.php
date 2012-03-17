92accf465e0b61e4070fb184b88c6f20c2acf3bc
Romain Neutron <imprec@gmail.com>
2012-03-17 10:15:20 +0100

    Refactor  Media Guesser

--- src/MediaVorus/Media.php
+++ src/MediaVorus/Media.php
@@ -39,85 +39,21 @@ class Media
       $file = new SymfonyFile($file->getPathname());
     }
 
+    $mime = $file->getMimeType();
 
-    switch ($file->getMimeType())
+    switch ($mime)
     {
-
-      case 'image/png':
-      case 'image/gif':
-      case 'image/bmp':
-      case 'image/x-ms-bmp':
-      case 'image/jpeg':
-      case 'image/pjpeg':
-      case 'image/psd':
-      case 'image/photoshop':
-      case 'image/vnd.adobe.photoshop':
-      case 'image/ai':
-      case 'image/illustrator':
-      case 'image/vnd.adobe.illustrator':
-      case 'image/tiff':
-      case 'image/x-photoshop':
+      case strpos($mime, 'image/') === 0:
       case 'application/postscript':
-      case 'image/x-tika-canon':
-      case 'image/x-canon-cr2':
-      case 'image/x-tika-casio':
-      case 'image/x-tika-dng':
-      case 'image/x-tika-epson':
-      case 'image/x-tika-fuji':
-      case 'image/x-tika-hasselblad':
-      case 'image/x-tika-imacon':
-      case 'image/x-tika-kodak':
-      case 'image/x-tika-leaf':
-      case 'image/x-tika-logitech':
-      case 'image/x-tika-mamiya':
-      case 'image/x-tika-minolta':
-      case 'image/x-tika-nikon':
-      case 'image/x-tika-olympus':
-      case 'image/x-tika-panasonic':
-      case 'image/x-tika-pentax':
-      case 'image/x-tika-phaseone':
-      case 'image/x-tika-rawzor':
-      case 'image/x-tika-red':
-      case 'image/x-tika-sigma':
-      case 'image/x-tika-sony':
-      case 'image/x-portable-pixmap':
         return new Media\Image($file, new \PHPExiftool\Exiftool);
         break;
 
-      case 'video/mpeg':
-      case 'video/mp4':
-      case 'video/x-ms-wmv':
-      case 'video/x-ms-wmx':
-      case 'video/avi':
-      case 'video/mp2p':
-      case 'video/mp4':
-      case 'video/x-ms-asf':
-      case 'video/quicktime':
-      case 'video/matroska':
-      case 'video/x-msvideo':
-      case 'video/x-ms-video':
-      case 'video/x-flv':
-      case 'video/avi':
-      case 'video/3gpp':
-      case 'video/x-m4v':
+      case strpos($mime, 'video/') === 0:
       case 'application/vnd.rn-realmedia':
+        return new Media\Video($file, new \PHPExiftool\Exiftool);
         break;
 
-      case 'audio/aiff':
-      case 'audio/aiff':
-      case 'audio/x-mpegurl':
-      case 'audio/mid':
-      case 'audio/mid':
-      case 'audio/mpeg':
-      case 'audio/ogg':
-      case 'audio/mp4':
-      case 'audio/scpls':
-      case 'audio/vnd.rn-realaudio':
-      case 'audio/x-pn-realaudio':
-      case 'audio/wav':
-      case 'audio/x-wav':
-      case 'audio/x-ms-wma':
-      case 'audio/x-flac':
+      case strpos($mime, 'audio/') === 0:
         break;
 
       case 'text/plain':

51a5d3140657d102def30ff69b7e33b6b8d7a478
Romain Neutron <imprec@gmail.com>
2012-05-06 15:40:09 +0200

    Refactor licensing terms

--- src/MediaVorus/MediaCollection.php
+++ src/MediaVorus/MediaCollection.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of MediaVorus.
+ *
+ * (c) 2012 Romain Neutron <imprec@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace MediaVorus;
 
 use \Doctrine\Common\Collections\ArrayCollection;
